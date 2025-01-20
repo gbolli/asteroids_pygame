620d7f73eb652e14e15dee7244f6461c5c56e6e8
Gord Bolli <gord@bolli.ca>
2025-01-20 10:17:52 -0800

    set up game clock / FPS

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         pygame.Surface.fill(screen, (0,0,0))
@@ -16,6 +18,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
